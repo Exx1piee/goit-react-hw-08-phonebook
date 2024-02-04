a0c5c880870d59d3d0abba26d3be7a3058e157cd
Exx1piee <mishapron2008@gmail.com>
2024-02-04 12:35:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+  <Provider store={store}>
+    <BrowserRouter basename='/goit-react-hw-08-phonebook'>
+    <App />
+    </BrowserRouter>
+  </Provider>
   </React.StrictMode>
 );
